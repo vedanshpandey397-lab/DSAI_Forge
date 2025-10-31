399e836b2dc153c97bc5c3ade6a9e23257934206
vedansh pandey <vedanshpandey@vedanshs-MacBook-Air.local>
2025-10-31 10:05:41 +0530

    Fixed loops and formatted code

---
+++ day5_loops.py
@@ -0,0 +1,17 @@
+nums =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for val in nums:
+    print("number:", val, "square:", val*val)
+
+i = 1
+num= int(input("enter a number"))
+
+while i<=10:
+    print(num, "X", i, "=", i*num)
+    i+=1
+
+
+k=1 
+while k<=10:
+    print("i will not skip my daily forge again")
+    k+=1
